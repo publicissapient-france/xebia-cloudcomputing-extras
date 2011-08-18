a8af77bd5066c16da99c1e19478677cc172cc330
Cyrille Le Clerc <cleclerc@xebia.fr>
2011-08-18 09:55:55 +0000

    formatting
    git-svn-id: https://xebia-france.googlecode.com/svn/cloudcomputing/xebia-cloudcomputing-extras/trunk@2041 63a6d7ea-0d3b-0410-ae18-9770a892219d

--- src/test/java/fr/xebia/demo/amazon/aws/SelfSignedX509CertificateGeneratorDemo.java
+++ src/test/java/fr/xebia/demo/amazon/aws/SelfSignedX509CertificateGeneratorDemo.java
@@ -46,75 +46,75 @@ import com.google.common.base.Strings;
  */
 public class SelfSignedX509CertificateGeneratorDemo {
 
-static {
-    // adds the Bouncy castle provider to java security
-    Security.addProvider(new BouncyCastleProvider());
-}
+    static {
+        // adds the Bouncy castle provider to java security
+        Security.addProvider(new BouncyCastleProvider());
+    }
 
-/**
- * <p>
- * Generate a self signed X509 certificate .
- * </p>
- * <p>
- * TODO : do the same with
- * {@link org.bouncycastle.cert.X509v1CertificateBuilder} instead of the
- * deprecated {@link org.bouncycastle.x509.X509V1CertificateGenerator}.
- * </p>
- */
-@SuppressWarnings("deprecation")
-static void generateSelfSignedX509Certificate() throws NoSuchAlgorithmException, NoSuchProviderException, CertificateEncodingException,
-        SignatureException, InvalidKeyException, IOException {
+    /**
+     * <p>
+     * Generate a self signed X509 certificate .
+     * </p>
+     * <p>
+     * TODO : do the same with
+     * {@link org.bouncycastle.cert.X509v1CertificateBuilder} instead of the
+     * deprecated {@link org.bouncycastle.x509.X509V1CertificateGenerator}.
+     * </p>
+     */
+    @SuppressWarnings("deprecation")
+    static void generateSelfSignedX509Certificate() throws NoSuchAlgorithmException, NoSuchProviderException, CertificateEncodingException,
+            SignatureException, InvalidKeyException, IOException {
 
-    // yesterday
-    Date validityBeginDate = new Date(System.currentTimeMillis() - 24 * 60 * 60 * 1000);
-    // in 2 years
-    Date validityEndDate = new Date(System.currentTimeMillis() + 2 * 365 * 24 * 60 * 60 * 1000);
+        // yesterday
+        Date validityBeginDate = new Date(System.currentTimeMillis() - 24 * 60 * 60 * 1000);
+        // in 2 years
+        Date validityEndDate = new Date(System.currentTimeMillis() + 2 * 365 * 24 * 60 * 60 * 1000);
 
-    // GENERATE THE PUBLIC/PRIVATE RSA KEY PAIR
-    KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA", "BC");
-    keyPairGenerator.initialize(1024, new SecureRandom());
+        // GENERATE THE PUBLIC/PRIVATE RSA KEY PAIR
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA", "BC");
+        keyPairGenerator.initialize(1024, new SecureRandom());
 
-    KeyPair keyPair = keyPairGenerator.generateKeyPair();
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
 
-    // GENERATE THE X509 CERTIFICATE
-    X509V1CertificateGenerator certGen = new X509V1CertificateGenerator();
-    X500Principal dnName = new X500Principal("CN=John Doe");
+        // GENERATE THE X509 CERTIFICATE
+        X509V1CertificateGenerator certGen = new X509V1CertificateGenerator();
+        X500Principal dnName = new X500Principal("CN=John Doe");
 
-    certGen.setSerialNumber(BigInteger.valueOf(System.currentTimeMillis()));
-    certGen.setSubjectDN(dnName);
-    certGen.setIssuerDN(dnName); // use the same
-    certGen.setNotBefore(validityBeginDate);
-    certGen.setNotAfter(validityEndDate);
-    certGen.setPublicKey(keyPair.getPublic());
-    certGen.setSignatureAlgorithm("SHA256WithRSAEncryption");
+        certGen.setSerialNumber(BigInteger.valueOf(System.currentTimeMillis()));
+        certGen.setSubjectDN(dnName);
+        certGen.setIssuerDN(dnName); // use the same
+        certGen.setNotBefore(validityBeginDate);
+        certGen.setNotAfter(validityEndDate);
+        certGen.setPublicKey(keyPair.getPublic());
+        certGen.setSignatureAlgorithm("SHA256WithRSAEncryption");
 
-    X509Certificate cert = certGen.generate(keyPair.getPrivate(), "BC");
+        X509Certificate cert = certGen.generate(keyPair.getPrivate(), "BC");
 
-    // DUMP CERTIFICATE AND KEY PAIR
-    System.out.println(Strings.repeat("=", 80));
-    System.out.println("CERTIFICATE TO_STRING");
-    System.out.println(Strings.repeat("=", 80));
-    System.out.println();
-    System.out.println(cert);
-    System.out.println();
+        // DUMP CERTIFICATE AND KEY PAIR
+        System.out.println(Strings.repeat("=", 80));
+        System.out.println("CERTIFICATE TO_STRING");
+        System.out.println(Strings.repeat("=", 80));
+        System.out.println();
+        System.out.println(cert);
+        System.out.println();
 
-    System.out.println(Strings.repeat("=", 80));
-    System.out.println("CERTIFICATE PEM (to store in a cert-johndoe.pem file)");
-    System.out.println(Strings.repeat("=", 80));
-    System.out.println();
-    PEMWriter pemWriter = new PEMWriter(new PrintWriter(System.out));
-    pemWriter.writeObject(cert);
-    pemWriter.flush();
-    System.out.println();
+        System.out.println(Strings.repeat("=", 80));
+        System.out.println("CERTIFICATE PEM (to store in a cert-johndoe.pem file)");
+        System.out.println(Strings.repeat("=", 80));
+        System.out.println();
+        PEMWriter pemWriter = new PEMWriter(new PrintWriter(System.out));
+        pemWriter.writeObject(cert);
+        pemWriter.flush();
+        System.out.println();
 
-    System.out.println(Strings.repeat("=", 80));
-    System.out.println("PRIVATE KEY PEM (to store in a priv-johndoe.pem file)");
-    System.out.println(Strings.repeat("=", 80));
-    System.out.println();
-    pemWriter.writeObject(keyPair.getPrivate());
-    pemWriter.flush();
-    System.out.println();
-}
+        System.out.println(Strings.repeat("=", 80));
+        System.out.println("PRIVATE KEY PEM (to store in a priv-johndoe.pem file)");
+        System.out.println(Strings.repeat("=", 80));
+        System.out.println();
+        pemWriter.writeObject(keyPair.getPrivate());
+        pemWriter.flush();
+        System.out.println();
+    }
 
     public static void main(String[] args) {
         try {
