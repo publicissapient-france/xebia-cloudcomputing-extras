a6ac975d950b1400dfd8a1a533bbd4a5f4347358
nicolas.demengel@gmail.com <nicolas.demengel@gmail.com@63a6d7ea-0d3b-0410-ae18-9770a892219d>
2011-09-11 17:56:12 +0000

    Adds PetclinicJenkinsJobCreator
    git-svn-id: https://xebia-france.googlecode.com/svn/cloudcomputing/xebia-cloudcomputing-extras/trunk@2126 63a6d7ea-0d3b-0410-ae18-9770a892219d

---
+++ src/main/java/fr/xebia/demo/amazon/aws/PetclinicJenkinsJobCreator.java
@@ -0,0 +1,114 @@
+package fr.xebia.demo.amazon.aws;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.collect.Maps.newHashMap;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import fr.xebia.cloud.cloudinit.FreemarkerUtils;
+
+/**
+ * Creates a job in a Jenkins server for a "Petclinic" project hosted at https://github.com/xebia-france-training/.
+ * 
+ * Example:
+ * 
+ * <pre>
+ * new PetclinicJenkinsJobCreator(&quot;http://ec2-46-137-62-232.eu-west-1.compute.amazonaws.com:8080&quot;).create(new PetclinicProjectInstance(&quot;42&quot;));
+ * </pre>
+ */
+public class PetclinicJenkinsJobCreator {
+
+    private static final Logger logger = LoggerFactory.getLogger(PetclinicJenkinsJobCreator.class);
+
+    private final String jenkinsUrl;
+
+    public PetclinicJenkinsJobCreator(String jenkinsUrl) {
+        this.jenkinsUrl = checkNotNull(jenkinsUrl);
+        checkArgument(jenkinsUrl.startsWith("http://"), "Invalid URL provided for Jenkins server: " + jenkinsUrl);
+    }
+
+    public void create(PetclinicProjectInstance project) {
+        checkNotNull(project);
+        String jobConfig = createConfig(project);
+        sendConfig(jobConfig, project);
+    }
+
+    private String createConfig(PetclinicProjectInstance project) {
+        Map<String, Object> parameters = newHashMap();
+        parameters.put("projectName", project.projectName);
+        parameters.put("groupId", project.groupId);
+        parameters.put("artifactId", project.artifactId);
+        return FreemarkerUtils.generate(parameters, "/petclinic-jenkins-job-config.xml.fmt");
+    }
+
+    private void sendConfig(String jobConfig, PetclinicProjectInstance project) {
+        HttpClient client = new DefaultHttpClient();
+        HttpEntity entity = httpEntityForXml(jobConfig);
+        HttpPost post = new HttpPost(jenkinsUrl + "/createItem?name=" + project.projectName);
+        post.setEntity(entity);
+
+        try {
+            if (logger.isDebugEnabled()) {
+                logger.debug("Executing request " + post.getRequestLine());
+            }
+
+            HttpResponse response;
+            try {
+                response = client.execute(post);
+            } catch (Exception e) {
+                throw new JobCreationException("Could not execute request", e);
+            }
+
+            if (logger.isDebugEnabled()) {
+                logger.debug("Response status: " + response.getStatusLine());
+            }
+
+            if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
+                if (logger.isWarnEnabled()) {
+                    try {
+                        logger.warn(EntityUtils.toString(response.getEntity()));
+                    } catch (Exception e) {
+                        logger.warn("Could not print entity");
+                    }
+                }
+                throw new JobCreationException(response.getStatusLine().toString());
+            }
+        } finally {
+            client.getConnectionManager().shutdown();
+        }
+    }
+
+    private HttpEntity httpEntityForXml(String string) {
+        try {
+            return new StringEntity(string, "text/xml", "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new JobCreationException("UTF-8 not supported by the platform?!", e);
+        }
+    }
+
+    public static class JobCreationException extends RuntimeException {
+
+        private static final long serialVersionUID = -559471701873832122L;
+
+        public JobCreationException(String message) {
+            super(message);
+        }
+
+        public JobCreationException(String message, Throwable cause) {
+            super(message, cause);
+        }
+    }
+}
