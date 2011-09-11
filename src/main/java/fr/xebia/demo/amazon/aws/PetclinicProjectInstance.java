0b7d5d58aa559f80535a5c46faa41062d268dce2
Nicolas Demengel <ndemengel@xebia.fr>
2011-09-11 17:56:12 +0000

    Adds PetclinicJenkinsJobCreator
    git-svn-id: https://xebia-france.googlecode.com/svn/cloudcomputing/xebia-cloudcomputing-extras/trunk@2126 63a6d7ea-0d3b-0410-ae18-9770a892219d

---
+++ src/main/java/fr/xebia/demo/amazon/aws/PetclinicProjectInstance.java
@@ -0,0 +1,21 @@
+package fr.xebia.demo.amazon.aws;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+/**
+ * Creates and holds the properties of a "Petclinic" project for a given team ID.
+ */
+public class PetclinicProjectInstance {
+
+    public final String groupId;
+    public final String artifactId;
+    public final String projectName;
+
+    public PetclinicProjectInstance(String teamId) {
+        checkArgument(teamId != null && !teamId.trim().isEmpty(), "Team ID must not be blank");
+
+        groupId = "fr.xebia.demo.petclinic-" + teamId;
+        artifactId = "xebia-petclinic";
+        projectName = artifactId + "-" + teamId;
+    }
+}
